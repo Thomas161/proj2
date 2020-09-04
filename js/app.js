c771db6a874c1ad3cac16faeb98e988d7b6d2187
Tom Magin <magintc@mac.com>
2020-09-04 16:27:55 +1000

    #js gsap effects for li and navbar

--- js/app.js
+++ js/app.js
@@ -1,17 +1,37 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   let startPerf = performance.now();
   console.log("Event fired", event);
-  var t1 = gsap.timeline();
-  var topHead = document.querySelector(".topHeader");
-  var shield = document.querySelector("#shield");
-  var head = document.getElementById("prac");
+  let t1 = gsap.timeline();
+  let topHead = document.querySelector(".topHeader");
+  let grid = document.querySelector(".gridNavbar");
+  let shield = document.querySelector("#shield");
+  let head = document.getElementById("prac");
 
   let playAnimation = function () {
     t1.fromTo(topHead, 2, { y: -200 }, { y: 0, delay: 1, ease: "bounce.out" });
     t1.fromTo(head, 0.7, { autoAlpha: 0 }, { autoAlpha: 1, delay: 1.5 });
+    t1.fromTo(grid, 0.7, { y: 600 }, { y: 0, delay: 1.5 });
     t1.fromTo(shield, 0.7, { scaleX: 0 }, { scaleX: 1, delay: 1 });
     t1.play();
   };
+
+  let items = document.querySelector(".navbarItems");
+  let individual = items.getElementsByTagName("li");
+  //   console.log(individual);
+
+  for (let i = 0; i < individual.length; i++) {
+    console.log("list of list items", individual[i]);
+    individual[i].addEventListener("mouseover", function (event) {
+      event.target.style.color = "red";
+    });
+  }
+  for (let i = 0; i < individual.length; i++) {
+    console.log("list of list items", individual[i]);
+    individual[i].addEventListener("mouseout", function (event) {
+      event.target.style.color = "white";
+    });
+  }
+
   playAnimation();
   let endPerf = performance.now();
   console.log(`Entire time in milliseconds : ${endPerf - startPerf} ms`);
