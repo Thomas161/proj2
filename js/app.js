fc32076050245d764647773c3ffac51743381cdd
Tom Magin <magintc@mac.com>
2020-09-04 15:15:22 +1000

    #responsive styling

--- js/app.js
+++ js/app.js
@@ -2,10 +2,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let startPerf = performance.now();
   console.log("Event fired", event);
   var t1 = gsap.timeline();
+  var topHead = document.querySelector(".topHeader");
+  var shield = document.querySelector("#shield");
   var head = document.getElementById("prac");
 
   let playAnimation = function () {
-    t1.fromTo(head, 0.9, { autoAlpha: 0 }, { autoAlpha: 1, delay: 1 });
+    t1.fromTo(topHead, 2, { y: -200 }, { y: 0, delay: 1, ease: "bounce.out" });
+    t1.fromTo(head, 0.7, { autoAlpha: 0 }, { autoAlpha: 1, delay: 1.5 });
+    t1.fromTo(shield, 0.7, { scaleX: 0 }, { scaleX: 1, delay: 1 });
     t1.play();
   };
   playAnimation();
