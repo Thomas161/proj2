12917ea31fe1611e0bb2731dfa69f43090d71ad0
Tom Magin <magintc@mac.com>
2020-09-07 15:37:49 +1000

    #working on images animation

--- js/app.js
+++ js/app.js
@@ -2,16 +2,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let startPerf = performance.now();
   console.log("Event fired", event);
   let t1 = gsap.timeline();
+  // let imageTimeline = gsap.timeline();
   let topHead = document.querySelector(".topHeader");
   let grid = document.querySelector(".gridNavbar");
+  let slides = document.querySelector(".mySlides");
   let shield = document.querySelector("#shield");
   let head = document.getElementById("prac");
+  let raids = document.getElementById("raider_pic");
+  let dufty = document.getElementById("dufty_pic");
+  let lockyer = document.getElementById("lockyer_pic");
 
   let playAnimation = function () {
     t1.fromTo(topHead, 2, { y: -200 }, { y: 0, delay: 1, ease: "bounce.out" });
     t1.fromTo(head, 0.7, { autoAlpha: 0 }, { autoAlpha: 1, delay: 1.5 });
-    t1.fromTo(grid, 0.7, { y: 600 }, { y: 0, delay: 1.5 });
-    t1.fromTo(shield, 0.7, { scaleX: 0 }, { scaleX: 1, delay: 1 });
+    t1.fromTo(grid, 0.7, { y: 600 }, { y: 0, delay: 1.3 });
+    t1.fromTo(shield, 0.7, { scaleX: 0 }, { scaleX: 1, delay: 0.5 });
+    t1.fromTo(slides, 0.7, { autoAlpha: 0 }, { autoAlpha: 1, delay: 0.7 });
+
+    t1.fromTo(raids, 0.7, { x: 500 }, { x: 0, opacity: 1, delay: 0.7 });
+    t1.to(raids, 0.7, { x: -500, delay: 2 });
+    t1.fromTo(dufty, 0.7, { x: 500 }, { x: 0, opacity: 1, delay: 0.7 });
+    t1.to(dufty, 0.7, { x: -500, delay: 2 });
+    t1.fromTo(lockyer, 0.7, { x: 500 }, { x: 0, opacity: 1, delay: 0.7 });
+    t1.to(lockyer, 0.7, { x: -500, delay: 2 });
     t1.play();
   };
 
@@ -50,6 +63,21 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
 
   playAnimation();
+  let slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    let index;
+    let slides = document.getElementsByClassName("mySlides");
+    for (index = 0; index < slides.length; index++) {
+      slides[index].style.display = "none";
+    }
+
+    slideIndex++;
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000);
+  }
+
   let endPerf = performance.now();
   console.log(`Entire time in milliseconds : ${endPerf - startPerf} ms`);
 });
