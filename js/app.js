c052e49367e93e3ba6b04960f03bdd9f34d0713e
Tom Magin <magintc@mac.com>
2020-09-21 14:58:31 +1000

    #fixing up some content

--- js/app.js
+++ js/app.js
@@ -6,28 +6,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let grid = document.querySelector(".navbar");
   let slides = document.querySelector(".slideshowContainer");
   let shield = document.querySelector("#shield");
-  let one = document.getElementById("first");
-  let second = document.getElementById("second");
-  let third = document.getElementById("third");
-  let fourth = document.getElementById("fourth");
-  let fifth = document.getElementById("fifth");
-  let sixth = document.getElementById("sixth");
-  let seventh = document.getElementById("seventh");
-  let eigth = document.getElementById("eigth");
-  let ninth = document.getElementById("ninth");
-  let tenth = document.getElementById("tenth");
-  let eleventh = document.getElementById("eleventh");
-  let twelfth = document.getElementById("twelfth");
-  let thirteenth = document.getElementById("thirteenth");
-  let fourteenth = document.getElementById("fourteenth");
-  let fifteenth = document.getElementById("fifteenth");
-  let sixteenth = document.getElementById("sixteenth");
-  let seventeenth = document.getElementById("seventeenth");
-  let eighteenth = document.getElementById("eighteenth");
-  let nineteenth = document.getElementById("nineteenth");
-  let twentieth = document.getElementById("twentieth");
-  let twentyfirst = document.getElementById("twentyfirst");
-  // let foot = document.querySelector(".footerContainer");
+
   let chart = document.getElementById("premiershipTable");
 
   let playAnimation = function () {
@@ -42,258 +21,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     t1.fromTo(slides, 0.7, { autoAlpha: 0 }, { autoAlpha: 1, delay: 0.4 });
 
     t1.fromTo(chart, 0.7, { scaleY: 0 }, { scaleY: 1, delay: 0.4 });
-    t1.staggerFromTo(
-      one,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      second,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      third,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      fourth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      fifth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      sixth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      seventh,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      eigth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      ninth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      tenth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      eleventh,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      twelfth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      thirteenth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      fourteenth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      fifteenth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      sixteenth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      seventeenth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      eighteenth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      nineteenth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      twentieth,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
-    t1.staggerFromTo(
-      twentyfirst,
-
-      0.3,
-      { y: 30, opacity: 0 },
-      {
-        y: 0,
-        opacity: 1,
-        transformOrigin: "50% 50%",
-        stagger: 0.2,
-      }
-    );
 
     t1.play();
   };
@@ -376,31 +103,344 @@ document.addEventListener("DOMContentLoaded", function (event) {
   }
   addActiveClass();
 
-  // let timelineLogo = gsap.timeline();
-  // const hovericon = () => {
-  //   timelineLogo.fromTo(
-  //     nrlLogo,
-  //     0.2,
-  //     { scale: 1 },
-  //     { scale: 1.1, boxShadow: "5px 10px 8px #888888" }
-  //   );
-  //   timelineLogo.play();
-  // };
-  // const hovericonOut = () => {
-  //   timelineLogo.fromTo(
-  //     nrlLogo,
-  //     0.2,
-  //     { scale: 1.1 },
-  //     { scale: 1, boxShadow: "none" }
-  //   );
-  //   timelineLogo.play();
-  // };
-  // const nrlLogo = document.getElementById("nrl");
-  // nrlLogo.addEventListener("mouseover", hovericon, true);
-  // nrlLogo.addEventListener("mouseout", hovericonOut, true);
+  let timelineLogo = gsap.timeline();
+  const hovericon = () => {
+    timelineLogo.fromTo(
+      nrlLogo,
+      0.2,
+      { scale: 1 },
+      { scale: 1.1, boxShadow: "5px 10px 8px #888888" }
+    );
+    timelineLogo.play();
+  };
+  const hovericonOut = () => {
+    timelineLogo.fromTo(
+      nrlLogo,
+      0.2,
+      { scale: 1.1 },
+      { scale: 1, boxShadow: "none" }
+    );
+    timelineLogo.play();
+  };
+  const nrlLogo = document.getElementById("nrl");
+  nrlLogo.addEventListener("mouseover", hovericon, true);
+  nrlLogo.addEventListener("mouseout", hovericonOut, true);
 
   // hovericon();
 
   let endPerf = performance.now();
   console.log(`Entire time in milliseconds : ${endPerf - startPerf} ms`);
 });
+/**************************************************************************************************** */
+/**UNUSED CODE SNIPPETS */
+
+/** 
+ * 
+  let one = document.getElementById("first");
+  let second = document.getElementById("second");
+  let third = document.getElementById("third");
+  let fourth = document.getElementById("fourth");
+  let fifth = document.getElementById("fifth");
+  let sixth = document.getElementById("sixth");
+  let seventh = document.getElementById("seventh");
+  let eigth = document.getElementById("eigth");
+  let ninth = document.getElementById("ninth");
+  let tenth = document.getElementById("tenth");
+  let eleventh = document.getElementById("eleventh");
+  let twelfth = document.getElementById("twelfth");
+  let thirteenth = document.getElementById("thirteenth");
+  let fourteenth = document.getElementById("fourteenth");
+  let fifteenth = document.getElementById("fifteenth");
+  let sixteenth = document.getElementById("sixteenth");
+  let seventeenth = document.getElementById("seventeenth");
+  let eighteenth = document.getElementById("eighteenth");
+  let nineteenth = document.getElementById("nineteenth");
+  let twentieth = document.getElementById("twentieth");
+  let twentyfirst = document.getElementById("twentyfirst");
+  let foot = document.querySelector(".footerContainer");
+
+    t1.staggerFromTo(
+       one,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       second,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       third,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       fourth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       fifth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       sixth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       seventh,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       eigth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       ninth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       tenth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       eleventh,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       twelfth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       thirteenth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       fourteenth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       fifteenth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       sixteenth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       seventeenth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       eighteenth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       nineteenth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       twentieth,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+         y: 0,
+         opacity: 1,
+         transformOrigin: "50% 50%",
+         stagger: 0.2,
+       }
+     );
+     t1.staggerFromTo(
+       twentyfirst,
+
+       0.3,
+       { y: 30, opacity: 0 },
+       {
+     y: 0,
+     opacity: 1,
+     transformOrigin: "50% 50%",
+     stagger: 0.2,
+   }
+ );
+
+fution changeBackground() {
+   console.dir("Event fired", event);
+  let secondColumn, pTags;
+  try {
+    secondColumn = document.querySelector("#separate");
+    const list = ["colTwo", "changeFontColorOver"];
+    secondColumn.classList.add(...list);
+    pTags = document.getElementsByTagName("p");
+    for (let i = 0; i < pTags.length; i++) {
+      pTags[i].style.color = "#fff";
+    }
+  } catch (error) {
+    console.log(error.stack);
+  }
+  return secondColumn, pTags;
+}
+function changeBack() {
+  // console.log("Event fired", event);
+  let secondColumn, pTags;
+  try {
+    secondColumn = document.querySelector("#separate");
+    secondColumn.classList.remove("colTwo");
+    pTags = document.querySelectorAll("p");
+    for (let i = 0; i < pTags.length; i++) {
+      pTags[i].style.color = "#000";
+    }
+  } catch (error) {
+    console.log(error.stack);
+  }
+  return secondColumn, pTags;
+}
+*/
